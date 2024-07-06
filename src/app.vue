7064aabfd5df0676be2aaac63bbbb54fe7d92d2e
pythonPlant12 <pythonplant12@gmail.com>
2024-07-06 22:46:08 +0200

    Adding some SEO meta tags to nuxt config

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,15 @@
 <template>
   <NuxtLayout>
-			<v-app>
-				<NuxtPage />
-			</v-app>
-		</NuxtLayout>
+    <v-app>
+      <NuxtPage/>
+    </v-app>
+  </NuxtLayout>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'NT Solutions',
+  ogTitle: 'NT Solutions',
+  description: 'Soluciones informáticas para tu empresa. Desarrollo de software, diseño web, marketing digital y más. Creamos tu presencia en el mundo digital.',
+  ogDescription: 'Soluciones informáticas para tu empresa. Desarrollo de software, diseño web, marketing digital y más. Creamos tu presencia en el mundo digital.',
+})
+</script>
--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -8,10 +8,6 @@ export default defineNuxtConfig({
       meta: [
         {charset: 'utf-8'},
         {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-        {
-          name: 'description',
-          content: 'Soluciones informáticas para tu empresa. Desarrollo de software, diseño web, marketing digital y más. Creamos tu presencia en el mundo digital.'
-        },
       ],
       link: [
         {rel: 'icon', type: 'image/x-icon', href: '/favicon.png'},
