fbe1b0eca023310d1022db572696086a7b2d6f8b
pythonPlant12 <pythonplant12@gmail.com>
2024-07-28 18:38:35 +0200

    Trying connection with backend-service

--- src/components/contact-us/form.vue
+++ src/components/contact-us/form.vue
@@ -215,7 +215,7 @@ export default {
         quantity: 10,
       },
     ],
-    url1: "http://nt-solutions-backend:10000/testapp/",
+    url1: "https://nt-solutions-backend:10000/testapp/",
     url2: "https://nt-solutions-backend.onrender.com/testapp/",
     url3: "http://127.0.0.1:8000/testapp/"
   }),
