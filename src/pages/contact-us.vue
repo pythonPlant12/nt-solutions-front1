44d49dc5836df6ee6d78d3be63075b527c9aea9d
pythonPlant12 <pythonplant12@gmail.com>
2024-08-01 16:47:56 +0200

    Thanks for contacting us in contact-us component

--- src/pages/contact-us.vue
+++ src/pages/contact-us.vue
@@ -16,15 +16,39 @@
         v-model="dialog"
         width="auto"
     >
+      <!--      <v-card-->
+      <!--          max-width="400"-->
+      <!--          prepend-icon="mdi-update"-->
+      <!--          text="Proximamente nos pondremos en contacto con usted para darle seguimiento a su solicitud."-->
+      <!--          title="Solicitud enviada"-->
+      <!--      >-->
+      <!--        <template v-slot:actions>-->
+      <!--          <v-btn-->
+      <!--              class="ms-auto"-->
+      <!--              text="Ok"-->
+      <!--              @click="dialog = false"-->
+      <!--          ></v-btn>-->
+      <!--        </template>-->
+      <!--      </v-card>-->
       <v-card
+          class="mx-auto"
+          prepend-icon="$vuetify"
+          subtitle="Gracias por ponerse en contacto"
           max-width="400"
-          prepend-icon="mdi-update"
-          text="Proximamente nos pondremos en contacto con usted para darle seguimiento a su solicitud."
-          title="Solicitud enviada"
       >
+        <template v-slot:title>
+          <span class="font-weight-black" style="color: var(--blue)">Solicitud enviada</span>
+        </template>
+
+        <v-card-text class="bg-surface-light pt-4">
+          Proximamente nos pondremos en contacto con usted para darle seguimiento a su solicitud.
+        </v-card-text>
+
         <template v-slot:actions>
           <v-btn
+              style="color: var(--blue)"
               class="ms-auto"
+              variant="tonal"
               text="Ok"
               @click="dialog = false"
           ></v-btn>
