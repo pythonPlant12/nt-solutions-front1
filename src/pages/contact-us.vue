3120203d72d2a6cee0e772c6e4c8bcb4b42e9084
pythonPlant12 <pythonplant12@gmail.com>
2024-08-03 15:53:12 +0200

    Disabling log in and sign up and making warning

--- src/pages/contact-us.vue
+++ src/pages/contact-us.vue
@@ -16,23 +16,9 @@
         v-model="dialog"
         width="auto"
     >
-      <!--      <v-card-->
-      <!--          max-width="400"-->
-      <!--          prepend-icon="mdi-update"-->
-      <!--          text="Proximamente nos pondremos en contacto con usted para darle seguimiento a su solicitud."-->
-      <!--          title="Solicitud enviada"-->
-      <!--      >-->
-      <!--        <template v-slot:actions>-->
-      <!--          <v-btn-->
-      <!--              class="ms-auto"-->
-      <!--              text="Ok"-->
-      <!--              @click="dialog = false"-->
-      <!--          ></v-btn>-->
-      <!--        </template>-->
-      <!--      </v-card>-->
       <v-card
           class="mx-auto"
-          prepend-icon="$vuetify"
+          prepend-icon="mdi-card-account-mail"
           subtitle="Gracias por ponerse en contacto"
           max-width="400"
       >
@@ -40,8 +26,8 @@
           <span class="font-weight-black" style="color: var(--blue)">Solicitud enviada</span>
         </template>
 
-        <v-card-text class="bg-surface-light pt-4">
-          Proximamente nos pondremos en contacto contigo para darle seguimiento a su solicitud.
+        <v-card-text class="pt-4">
+          Proximamente contactaremos contigo para darle seguimiento a tu solicitud.
         </v-card-text>
 
         <template v-slot:actions>
